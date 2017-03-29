3e52b7cdbc4c5a9d97f0c01f495a130d2a969cfa
Andrey Helldar <helldar@ai-rus.com>
2017-03-29 16:48:44 +0900

    Add a "account.getActiveOffers" method

---
+++ src/Controllers/Account/GetActiveOffersController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Helldar\Vk\Controllers\Account;
+
+use Helldar\Vk\Controllers\Controller;
+
+/**
+ * Returns a list of active ads (offers) which executed by the user will bring him/her respective number of votes to his balance in the
+ * application. To show the user one or all ads you can use showLeadsPaymentBox JS API method. After the user completed one of the
+ * campaigns offer to him/her, votes will be automatically entered to user's balance in the application. In this case, the application can
+ * write off the required number of votes by converting them into the application's internal currency.
+ *
+ * @see     https://vk.com/dev/account.getActiveOffers
+ *
+ * @author  Andrey Helldar <helldar@ai-rus.com>
+ * @package Helldar\Vk\Controllers\Account
+ */
+class GetActiveOffersController extends Controller
+{
+    /**
+     * Offset needed to return a specific subset of users.
+     * (positive Zahl, Voreinstellung 0)
+     *
+     * @author Andrey Helldar <helldar@ai-rus.com>
+     *
+     * @since  2017-03-29
+     *
+     * @param int $offset
+     *
+     * @return $this
+     */
+    public function offset($offset = 0)
+    {
+        $this->setParameter('offset', abs((int) $offset));
+
+        return $this;
+    }
+
+    /**
+     * Number of results to return.
+     *
+     * @author Andrey Helldar <helldar@ai-rus.com>
+     *
+     * @since  2017-03-29
+     *
+     * @param int $count
+     *
+     * @return $this
+     */
+    public function count($count = 100)
+    {
+        $this->setParameter('count', abs((int) $count));
+
+        return $this;
+    }
+}
