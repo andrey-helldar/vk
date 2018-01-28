13b8ea492a0af9049a5173a1122346e4a256c4ca
Dronax <fantommd5@gmail.com>
2018-01-28 12:46:21 +0200

    Create Messages.php

---
+++ src/Facade/Messages.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Helldar\Vk\Facade;
+
+class Messages extends BaseFacade
+{
+    /**
+     * @var string
+     */
+    protected $section = 'messages';
+    /**
+     * Available methods.
+     *
+     * @var array
+     */
+    protected $methods = array('send');
+}
